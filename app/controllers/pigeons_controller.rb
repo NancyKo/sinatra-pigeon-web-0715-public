f7bd6ef9fda4900ff08950f5bb39a0c27c3e9bf9
NancyKo <konancys@gmail.com>
2015-08-29 19:26:46 -0400

    done. tests pass but site broken :(

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -1,2 +1,43 @@
 # create your PigeonsController here
 # it should inherit from ApplicationController
+
+class PigeonsController < ApplicationController 
+
+  get '/pigeons' do
+    @pigeon = Pigeon.all 
+    erb :'/pigeons/index'
+  end
+
+  get '/pigeons/new' do
+    erb :'/pigeons/new'
+  end
+
+  get '/pigeons/:id' do
+    @pigeon = Pigeon.find(params[:id])
+    erb :'/pigeons/show'
+  end
+
+  get '/pigeons/:id/edit' do
+    @pigeon = Pigeon.find(params[:id])
+    erb :'/pigeons/edit'
+  end
+
+  patch '/pigeons/:id' do
+    @pigeon = Pigeon.find(params[:id])
+    # binding.pry
+    @pigeon.update(name: params[:name], color: params[:color], gender: params[:gender], lives: params[:lives])  
+    redirect :"/pigeons/#{@pigeon.id}"
+  end
+
+  post '/pigeons' do
+    @pigeon = Pigeon.create(params)
+    redirect :'/pigeons'
+  end
+
+  delete '/pigeons/:id' do 
+    @pigeon = Pigeon.find(params[:id])
+    @pigeon.destroy
+    redirect :'/pigeons'
+  end
+
+end
